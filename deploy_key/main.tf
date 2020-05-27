97808978760264fdb65579e5ce1df6dbe7e0b1c9
scott.eppler <scott.eppler@coda.global>
2020-05-27 11:13:08 -0500

    Move 'private_key' to an Output

--- deploy_key/main.tf
+++ deploy_key/main.tf
@@ -1,14 +1,9 @@
 provider "github" {
-  individual = "true"
+  individual = true
 }
 resource "tls_private_key" "deploy_key" {
   algorithm = "RSA"
   rsa_bits  = "4096"
-}
-
-resource "local_file" "private_key" {
-  content  = tls_private_key.deploy_key.private_key_pem
-  filename = "${path.root}/generated_keys/${var.name}_deploy_key.pem"
 }
 
 resource "github_repository_deploy_key" "repo_deploy_key" {
