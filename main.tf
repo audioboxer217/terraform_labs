d03e60beb18d7aec63efc6f1a35f31f0ec896e33
scott.eppler <scott.eppler@coda.global>
2020-05-27 10:38:32 -0500

    Update main.tf with Remote Backend

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "kseppler"
+
+    workspaces {
+      prefix = "terraform_labs_"
+    }
+  }
+}
+
 module "test_deploy_key" {
   source = "./deploy_key"
   name   = "test"
