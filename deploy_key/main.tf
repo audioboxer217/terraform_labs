2aa6603faf352ffb04f3e0b77ceb98704642cab5
scott.eppler <scott.eppler@coda.global>
2020-05-27 09:46:56 -0500

    Add 'deploy_key' module

---
+++ deploy_key/main.tf
@@ -0,0 +1,14 @@
+resource "tls_private_key" "deploy_key" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "local_file" "private_key" {
+  content  = tls_private_key.deploy_key.private_key_pem
+  filename = "${path.module}/generated_keys/${var.name}_deploy_key.pem"
+}
+
+resource "local_file" "public_key" {
+  content  = tls_private_key.deploy_key.public_key_openssh
+  filename = "${path.module}/generated_keys/${var.name}_deploy_key.pub"
+}
